c079a715a80f775c6d0da301bbd7dead1acf8e19
fazlullinrr <fazlullin@inbox.ru>
2021-06-01 11:51:41 +0300

    Решена первая задача

--- task_1.1/Main.java
+++ task_1.1/Main.java
@@ -15,6 +15,10 @@ name типа String, age типа int и city типа String.
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        String name;
+        int age;
+        String city;
+
+
     }
 }
