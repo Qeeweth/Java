38edc7e92fef37cd7df8116b0c097f2723614fc9
fazlullinrr <fazlullin@inbox.ru>
2021-06-02 18:04:00 +0300

    Решена задача пять

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -2,9 +2,17 @@
 Задание: Запросить у пользователя ввод числа и сохранить это число в переменную a.Если переменная a равна 10, то выведите 'Верно', иначе выведите 'Неверно'.
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a == 10) {
+            System.out.println("Верно");
+        }
+        else {
+            System.out.println("Не верно");
+        }
     }
 }
