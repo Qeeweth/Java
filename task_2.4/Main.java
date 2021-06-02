f8fdddc6db87ab226804460ce36daea64a119289
fazlullinrr <fazlullin@inbox.ru>
2021-06-02 17:53:15 +0300

    Решена задача четыре

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -1,10 +1,26 @@
 /*
 Задание: Пользователь вводит сумму вклада и процент, который будет начисляться ежегодно. Отобразить размер вклада поочередно на ближайшие 5 лет.
 */
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите размер вклада: ");
+        double deposit = scan.nextDouble();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите размер процента: ");
+        double percent = in.nextDouble();
+        int time = 5;
+        percent = percent / 100;
+        for (int i = 0; i < time; i++) {
+            deposit += deposit * percent;
+            System.out.println(deposit);
+
+
+
+
+        }
 
     }
 }
