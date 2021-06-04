56a1428cb3cf92359eb099eaba9eeaba3cbc8c95
fazlullinrr <fazlullin@inbox.ru>
2021-06-04 14:24:17 +0300

    Решена первая задача

--- task_3.1/Main.java
+++ task_3.1/Main.java
@@ -10,7 +10,13 @@
 
 public class Main {
     public static int min(int a, int b, int c) {
-        //напишите тут ваш код
+        if (a <= b && a <= c) {
+            return a;
+        } else if (b <=a && b <= c) {
+            return b;
+        } else {
+            return c;
+        }
     }
 
     public static void main(String[] args) {
