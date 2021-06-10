c45fde135ced3c59d8318abdbbdea1afd1a0cdfb
fazlullinrr <fazlullin@inbox.ru>
2021-06-10 13:28:50 +0300

    Решена задача два

---
+++ task_4.2/Dog.java
@@ -0,0 +1,25 @@
+public class Dog {
+    String name;
+    int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
--- task_4.2/Main.java
+++ task_4.2/Main.java
@@ -14,11 +14,30 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Dog dog = new Dog("Баки",7);
+        System.out.println(dog.getName());
+        System.out.println(dog.getAge());
     }
-}
+    public static class Dog {
+        private String name;
+        private int age;
+        public Dog(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return this.name;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public int getAge() {
+            return this.age;
+        }
+    }
 
-public class Dog {
-    //напишите тут ваш код
 }
 
