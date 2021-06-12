12dbd9c777275b6dcdb113e15701c205ce9ecb87
fazlullinrr <fazlullin@inbox.ru>
2021-06-12 22:41:56 +0300

    Решена задача пять

--- task_4.5/Main.java
+++ task_4.5/Main.java
@@ -8,6 +8,27 @@
 
 public class Main{
     public static void main(String[] args) {
-      //Ваш код
+        Horse horse = new Horse();
+        Pegasus pegasus = new Pegasus();
+        horse.run();
+        pegasus.fly();
+
     }
-}
+    abstract static class Animal {
+        public int age;
+        public String color;
+        public String breed;
+    }
+        static class Horse extends Animal {
+            public void run() {
+                System.out.println("Игого, я поскакал(а)");
+            }
+        }
+        static class Pegasus extends Horse {
+            public void fly() {
+                System.out.println("Игого, я полетел(а)");
+            }
+        }
+    }
+
+
