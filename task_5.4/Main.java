8dee0c0df6ba57185ea8e37ea5e6422552d2de50
fazlullinrr <fazlullin@inbox.ru>
2021-06-20 01:32:02 +0300

    Решена четвертая задача

--- task_5.4/Main.java
+++ task_5.4/Main.java
@@ -1,4 +1,4 @@
-  
+
 /* 
 1. Ввести путь к файлу с консоли.
 2. Прочитать из него набор чисел.
@@ -19,8 +19,31 @@
 10
 */
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
-    public static void main(String[] args) {
-        // напишите тут ваш код
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader = new BufferedReader(new InputStreamReader((new FileInputStream(fileName))));
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        while (reader.ready()) {
+            int num = Integer.parseInt(reader.readLine());
+            if (num % 2 == 0) {
+                numbers.add(num);
+            }
+        }
+        reader.close();
+        Collections.sort(numbers);
+
+        for (int i : numbers) {
+            System.out.println(i);
+        }
     }
 }
