252683e4212426b92591c02a7dd542ca96f457c0
fazlullinrr <fazlullin@inbox.ru>
2021-06-19 22:35:18 +0300

    Решена вторая задача

--- task_5.2/Main.java
+++ task_5.2/Main.java
@@ -13,34 +13,41 @@
 Абрамовичи */
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /* 
 Модернизация ПО
 */
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        List<String> list = new ArrayList<>();
+        List<String> cities = new ArrayList<>();
+        List<String> addresses = new ArrayList<String>();
         while (true) {
-            String family = scanner.nextLine();
-            if (family.isEmpty()) {
-                break;
-            }
-
-            list.add(family);
+            String city = reader.readLine();
+            if (city.isEmpty()) break;
+            String family = reader.readLine();
+            cities.add(city);
+            addresses.add(family);
         }
 
-        // Read the house number
-        int houseNumber = scanner.nextInt();
+        String cityIndex = reader.readLine();
 
-        if (0 <= houseNumber && houseNumber < list.size()) {
-            String familyName = list.get(houseNumber);
-            System.out.println(familyName);
+        for (int i = 0; i < cities.size(); i++) {
+            if (cityIndex.equals(cities.get(i))) {
+                System.out.println(addresses.get(i));
+            }
         }
     }
 }
+
+
+
+
+
